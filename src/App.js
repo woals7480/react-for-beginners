f9e5340b4af7d6ac304644d63f84e848d69d68dd
woals7480 <94383932+woals7480@users.noreply.github.com>
2022-03-10 16:30:30 +0900

    update react-for-beginners

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
-    </div>
-  );
+// function App() {
+//   const [counter, setValue] = useState(0)
+//   const [keyword, setKeyword] = useState("")
+//   const onClick = () =>{setValue((prev) => prev + 1)}
+//   const onChange = (event) => {setKeyword(event.target.value)}
+//   console.log("i run all the time")
+//   useEffect(()=>{console.log("CALL THE API...")}, []) //정렬에 아무것도 없으니까 한 번만 실행
+//   useEffect(()=>{
+//     if(keyword !== "" && keyword.length >= 5){console.log("SEARCH FOR ", keyword)}
+//   }, [keyword]) //keyword가 변화 될 때 마다 코드실행
+//   return (
+//     <div>
+//       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+//       <h1 className={styles.title}>{counter}</h1>
+//       <Button text={"Continue"}/>
+//       <button onClick={onClick}>click me</button>
+//     </div>
+//   );
+// }
+
+function Hello(){
+  useEffect(()=>{
+    console.log("created :)")
+    return () => console.log("destroyed:(")
+  },[])
+  return <h1>Hello</h1>
+}
+
+function App(){
+  const [showing, setShowing] = useState(false)
+  const onClick = () => {setShowing(prev => !prev)}
+  return(
+  <div>
+    {showing ? <Hello/> : null}
+    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+  </div>
+  )
+  
 }
 
 export default App;
